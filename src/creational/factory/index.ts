e879daf3ec22b5522e773a342a40c7a9b9cf157f
Gabriel Ávila <avilabiel@gmail.com>
2021-06-21 00:36:09 -0300

    so now we use namespaces

--- src/creational/factory/index.ts
+++ src/creational/factory/index.ts
@@ -1,39 +1,41 @@
-class RegularUser {
-  email: String;
-  role: String = 'regular';
-  privileges: String[] = ['page #1'];
+namespace Factory {
+  class RegularUser {
+    email: String;
+    role: String = 'regular';
+    privileges: String[] = ['page #1'];
 
-  constructor(email: String) {
-    this.email = email;
-  }
-}
-
-class AdminUser {
-  email: String;
-  role: String = 'admin';
-  privileges: String[] = ['page #1', 'page #2', 'page #3'];
-
-  constructor(email: String) {
-    this.email = email;
-  }
-}
-
-class UserFactory {
-  static build(email: String, role: String) {
-    if (role === 'admin') {
-      return new AdminUser(email);
+    constructor(email: String) {
+      this.email = email;
     }
-
-    if (role === 'regular') {
-      return new RegularUser(email);
-    }
-
-    throw new Error('User type not found');
   }
+
+  class AdminUser {
+    email: String;
+    role: String = 'admin';
+    privileges: String[] = ['page #1', 'page #2', 'page #3'];
+
+    constructor(email: String) {
+      this.email = email;
+    }
+  }
+
+  class UserFactory {
+    static build(email: String, role: String) {
+      if (role === 'admin') {
+        return new AdminUser(email);
+      }
+
+      if (role === 'regular') {
+        return new RegularUser(email);
+      }
+
+      throw new Error('User type not found');
+    }
+  }
+
+  const adminUser = UserFactory.build('admin@test.com', 'admin');
+  const regularUser = UserFactory.build('regular@test.com', 'regular');
+
+  console.log(adminUser);
+  console.log(regularUser);
 }
-
-const adminUser = UserFactory.build('admin@test.com', 'admin');
-const regularUser = UserFactory.build('regular@test.com', 'regular');
-
-console.log(adminUser.role, adminUser.privileges);
-console.log(regularUser.role, regularUser.privileges);
